402e303b1626b0e917fb12ff3b0da57dcfcbf631
younne0915 <younne@163.com>
2020-05-21 21:32:13 +0800

    DOF

---
+++ Assets/younne/Scripts/EffectSystem/Data/DepthOfFieldData.cs
@@ -0,0 +1,13 @@
+﻿using Sokkayo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sokkayo
+{
+    [System.Serializable]
+    public class DepthOfFieldData : EffectDataBase
+    {
+
+    }
+}
--- Assets/younne/Scripts/EffectSystem/DepthOfFieldEffect.cs
+++ Assets/younne/Scripts/EffectSystem/DepthOfFieldEffect.cs
@@ -8,25 +8,17 @@ namespace Sokkayo
 {
     public class DepthOfFieldEffect : EffectSystemBase
     {
-        protected override Shader _shader => Shader.Find("younne/BlurEffect");
-        private float _blurIntensity = 2;
-        private int iterator = 6;
-        private int MaxIterations = 6;
-        int[] _tempRenderTextureDownIds;
+        protected override Shader _shader => Shader.Find("younne/DepthOfField");
+        private DepthOfFieldData _depthOfFieldData;
 
         public DepthOfFieldEffect(Camera camera, EffectDataBase effectData) : base(camera, CameraEvent.AfterForwardOpaque)
         {
+            _depthOfFieldData = effectData as DepthOfFieldData;
         }
 
         protected override void Initialize()
         {
             base.Initialize();
-
-            _tempRenderTextureDownIds = new int[MaxIterations];
-            for (int i = 0; i < MaxIterations; i++)
-            {
-                _tempRenderTextureDownIds[i] = Shader.PropertyToID($"_BloomTempDown{i}");
-            }
         }
 
         public override void StartEffect()
@@ -35,52 +27,7 @@ namespace Sokkayo
 
             base.StartEffect();
 
-            _targetCamera.enabled = true;
-
-            if (_renderTex != null)
-            {
-                _renderTex.Release();
-            }
-
-            _renderTex = new RenderTexture(_targetCamera.pixelWidth, _targetCamera.pixelHeight, 0,
-                    RenderTextureFormat.Default, RenderTextureReadWrite.sRGB);
-            _renderTex.name = "RenderTex";
-            _renderTex.antiAliasing = 1;
-            _renderTex.Create();
-
-            var tempTex = new RenderTexture(_targetCamera.pixelWidth, _targetCamera.pixelHeight, 0,
-                   RenderTextureFormat.Default, RenderTextureReadWrite.sRGB);
-            tempTex.name = "TempTex";
-            tempTex.antiAliasing = 1;
-            tempTex.Create();
-
-            _targetCamera.targetTexture = tempTex;
-
-            _opaqueBuffer.Clear();
-            _opaqueBuffer.SetGlobalFloat(ShaderProperties.BlurSize, _blurIntensity);
-
-            RenderTargetIdentifier source = tempTex;
             
-            for (int i = 0; i < iterator; i++)
-            {
-                var mipDown = _tempRenderTextureDownIds[i];
-
-                _opaqueBuffer.GetTemporaryRT(mipDown,
-                    _targetCamera.pixelWidth, _targetCamera.pixelHeight,
-                    0, FilterMode.Bilinear,
-                    RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Linear);
-
-                _opaqueBuffer.Blit(source, mipDown, _mat, 0);
-                source = mipDown;
-            }
-
-            _opaqueBuffer.Blit(source, tempTex);
-
-            for (int i = 0; i < iterator; i++)
-            {
-                var mipDown = _tempRenderTextureDownIds[i];
-                _opaqueBuffer.ReleaseTemporaryRT(mipDown);
-            }
 
             if (_targetCamera != null)
             {
